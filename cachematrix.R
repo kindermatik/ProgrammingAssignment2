b2810a614dc839e4a802f7bc554f19a2faae7390
kindemartik <kindermatik@linux-mszb.site>
2014-04-23 18:05:59 +0300

    file cachematrix completed as per assignment instructions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,42 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(1:4,2)) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## makeCacheMatrix create a list with four functions
+## set-> inputs in cache the matrix included in the function 
+##    if nothing added default matrix is matrix (1:4,2)
+## get -> can be used to get matrix stored by set in step above
+## setinverse -> when cacheSolve is used, if the matrix is not saved in set
+##    it stores the inverse of the matrix (calculated as solve(matrix))
+## getinverse -> can be used to get the the matrix in setinverse
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+        ## Return a matrix that is the inverse of 'x'
+
+## to check that inverse is correctly calculated run
+## a$get()%*%a$getinverse()
+## if it is correct identity matrix should be returned
